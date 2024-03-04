a51defcbd647da98bf077d9a527f5fa8c42d2291
mintusikder <mintusikder15@gmail.com>
2024-03-04 14:55:40 +0600

    added break time

--- src/assets/components/SideCard.jsx
+++ src/assets/components/SideCard.jsx
@@ -5,11 +5,19 @@ import { useEffect, useState } from "react";
 const SideCard = ({ watchTime }) => {
 //   console.log(watchTime);
 const [time , setTime] = useState(watchTime)
-
+const [breakTime, setBreakTime] = useState(0)
   useEffect(()=>{
     const setWatchTime = localStorage.getItem("watchtime")
     setTime(setWatchTime)
+    const getBreakTime= localStorage.getItem("breakTime")
+    setBreakTime(getBreakTime)
   },[watchTime])
+
+
+  const handelBreakTime =(bt)=>{
+    localStorage.setItem("breakTime", bt)
+    setBreakTime(bt)
+  } 
   return (
     <div>
       <h1>My Cart</h1>
@@ -19,10 +27,10 @@ const [time , setTime] = useState(watchTime)
         <input type="text" value={time} disabled />
       </div>
       <h5 className="mt-5">Add Break time</h5>
-      <dutton className="w-25 btn-circle m-1 btn btn-info">15</dutton>
-      <dutton className="w-25 btn-circle bg-warning btn btn-info">20</dutton>
-      <dutton className="w-25 btn-circle m-1 bg-danger btn btn-info">25</dutton>
-      <input type="text" disabled />
+      <button onClick={()=>handelBreakTime(15)}  className="w-25 btn-circle m-1 btn btn-info">15</button>
+      <button className="w-25 btn-circle bg-warning btn btn-info">20</button>
+      <button className="w-25 btn-circle m-1 bg-danger btn btn-info">25</button>
+      <input type="text" value={breakTime} disabled />
       <button className="mt-5 btn btn-info w-100">Complete</button>
     </div>
   );
